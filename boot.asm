6026ec645b012ea72f4ff2b8010187de49241dba
Neal Sidhwaney <nealsid@users.noreply.github.com>
2022-11-08 19:23:14 -0800

    Add a third descriptor to GDT and update flags, as it seems that, under QEMU at least, a DS/ES/ES/FS are temporarily loaded with a segment selector for the 3rd segment in the GDT, even though after fully in protected mode, those registers are set to 0.

--- boot.asm
+++ boot.asm
@@ -1,7 +1,11 @@
 ; boot.asm
+bits 16
 org 0x7c00
 begin:
-        cli
+        mov ax, 0
+        mov ss, AX
+        mov sp, 0x7C00
+
         mov bx, hello_string
         call write_via_interrupt
 
@@ -14,12 +18,12 @@ begin:
         mov bx, boot_second_sector
         call write_via_interrupt
 
+        cli
         lgdt [lgdt_param]
-
         smsw cx
         or cx, 1
         lmsw cx
-        jmp dword 0x7c00:done
+        jmp 0x0008:protected_mode
 
 write_via_interrupt:
         mov ah, 0xE
@@ -88,7 +92,7 @@ boot_second_sector:
         db '2nd boot sector', 0xa, 0xd, 0
 
 lgdt_param:
-        db 0x0E, 0x00
+        db 0x17, 0x00
         dd gdt
 
         dw 0x1234
@@ -98,16 +102,23 @@ first_entry:
         dw 0x0000, 0x0000       ;First entry is unused
         dw 0x0000, 0x0000
 second_entry:
-        dw 0x0000FFFE
-        dw 0x00FF8000
-        ;; db 0x00                 ; bits 16-20 of base
-        ;; db 0x80                 ; type (unused for system descriptor?)
-        ;;                         ; 1000: present (bit 3), dpl = 1 (bits 2,1), system segment (bit 0)
-        ;; db 0xFF                 ; Upper 4 of segment limit
-        ;;                         ; Granularity, D/B, 64-bit (L), AVL all 1
-        ;; db 0x00                 ; upper 8 of base
+        dw 0xFFFF               ; segment limit 0-15
+        dw 0x0000               ; segment base 0-15
+        dw 0x9A00               ; base 16-23, followed by type == 1010 (execute/read),and
+                                ; flags = 9 = 1001 for present = 1,dpl=00,code/data = 1
+        dw 0x00CF               ; seg limit 16-19 = 0xF, flags = 1100
+                                ; granularity & default operation size
+                                ; (32bit) = 1, base 24-31 = 0
+third_entry:
+        dw 0xFFFF               ; same as above
+        dw 0x0000
+        dw 0x9A00
+        dw 0x00CF
 gdt_end:
 
+        bits 32
+protected_mode:
+        jmp protected_mode
         times 1022 - ($-$$) db 0
 second_sector_endmagic:
         db 0xAA
